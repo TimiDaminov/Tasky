841b8633a6216d29b52b4d8191fb42e0d7d784fd
Timur Daminov <timurdaminov55@gmail.com>
2022-12-28 00:36:21 +0200

    First commit!

--- App.js
+++ App.js
@@ -1,20 +1,73 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { View, TextInput, Button, StyleSheet, Text, Modal } from "react-native";
 
-export default function App() {
+const App = () => {
+  const [notes, setNotes] = useState([]);
+  const [currentNote, setCurrentNote] = useState("");
+  const [visibleModal, setVisibleModal] = useState(false);
+
+  const addNote = () => {
+    setNotes([...notes, currentNote]);
+    setVisibleModal(false);
+    setCurrentNote("");
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.appContainer}>
+      <Button onPress={() => setVisibleModal(true)} title="Add Note" />
+      <Button onPress={() => setNotes([])} title="clear" />
+      {notes.map((note, index) => (
+        <View key={index}>
+          <Text style={styles.notesItem}>
+            {index + 1}){note}
+          </Text>
+        </View>
+      ))}
+
+      {/* Modal Window  */}
+
+      <Modal animationType="slide" transparent={false} visible={visibleModal}>
+        <View style={styles.modalContainer}>
+          <TextInput
+            value={currentNote}
+            onChangeText={(text) => setCurrentNote(text)}
+            placeholder="Your Task..."
+            style={styles.noteAddInput}
+          />
+          <Button onPress={() => setVisibleModal(false)} title="close" />
+          <Button onPress={addNote} title="Add note" />
+        </View>
+      </Modal>
+
+      {/* modal window ends */}
     </View>
   );
-}
+};
 
+export default App;
 const styles = StyleSheet.create({
-  container: {
+  appContainer: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  notesItem: {
+    fontSize: 28,
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  noteAddInput: {
+    fontSize: 16,
+    fontWeight: "bold",
+    color: "#333",
+    borderWidth: 1,
+    borderColor: "#ddd",
+    borderRadius: 8,
+    padding: 14,
+    marginBottom: 10,
   },
 });
